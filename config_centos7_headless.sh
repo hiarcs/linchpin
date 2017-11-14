488ed52ef48d40a7615c8e7a19f6c3d5a1ff1bd9
Hiarcs <fey.young@gmail.com>
2017-11-14 13:46:09 +0800

    working on preparations

--- config_centos7_headless.sh
+++ config_centos7_headless.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/bash
 
+# 下载运行配置前，手动完成如下配置
+# 1. 接入互联网
+# 2. 生成rsa_key，更新authorized_keys及github/bitbucket的ssh密钥设置
+#
+
+su -c "yum install epel-release vim-e* git hg -y"
+su -c "yum update -y"
+
--- config_fedora26_headless.sh
+++ config_fedora26_headless.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/bash
 
+# 下载配置前，执行下列操作
+# 开启sshd，生成rsa_key并更新到authorized_keys以及github／bitbucket
+#
+# 安装git
+# 
+su -c "$HOME/tools/linchpin/linux/fedora26/root_01_basic.sh"
